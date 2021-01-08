a4150db9f96b3864efdc6351ffea235ec2b08a3b
Israel Curvelo <raellcurvelo@gmail.com>
2021-01-08 20:33:47 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Hello from './Hello';
+import Home from './Home';
 import Navbar from './Navbar';
 
 
@@ -8,8 +9,11 @@ import Navbar from './Navbar';
 function App() {
   return (
     <div >
+      <>
        <Navbar/>
-       
+       <Home/>
+      </>
+      
     </div>
     
   );
